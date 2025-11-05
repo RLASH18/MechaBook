88dbedca3a63f9109e2c0c4c04c0bfed1949781e
RLASH18 <lacdangryan18@gmail.com>
2025-11-05 19:44:07 +0800

    adjust design

--- resources/views/pages/employee/dashboard.blade.php
+++ resources/views/pages/employee/dashboard.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 @section('main')
     {{-- Header --}}
-    <x-page-header title="Dashboard">
-        Welcome back <strong class="text-blue-600">{{ auth()->user()->name }}</strong>!
-    </x-page-header>
-
+    <div class="flex items-center justify-between mb-4">
+        <div>
+            <x-page-header title="Dashboard">
+                Welcome back <strong class="text-blue-600">{{ auth()->user()->name }}</strong>!
+            </x-page-header>
+        </div>
+    </div>
 
 @endsection
